5d1a2f498248d28ae59f31ee2cce98a29470240b
emil <emilbabazade29@gmail.com>
2021-03-23 12:16:29 +0200

    setup sass

--- src/Carousel.js
+++ src/Carousel.js
@@ -1,11 +1,16 @@
 import React from 'react'
 
-const Carousel = ({src}) => (
-    <div>
-        <button>Previouse</button>
-        <img src={src}/>
+const Carousel = ({ src }) => {
+  const style = {
+    backgroundImage: `url("${src}")`
+  }
+
+  return (
+    <div style={style}>
+        <button>Previous</button>
         <button>Next</button>
     </div>
-)
+  )
+}
 
 export default Carousel
